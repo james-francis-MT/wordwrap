8cfb4220353539944a06ed417850a679d2179a2c
James Francis <james.franics@madetech.com>
2020-09-21 13:42:17 +0100

    sorting out master branch

--- lib/wordwrap.rb
+++ lib/wordwrap.rb
@@ -1,9 +1,28 @@
 class Wordwrap
     def word_wrap(string, width)
-        if string.length > width
-            return string.sub(/.*\K\s/, "\n")
+        if width < 1
+            return nil
         end
-        string
+
+        if string == nil
+            return ""
+        end
+
+        if string.length <= width
+            return string
+        else
+            space = string.index(" ")
+            if space != nil
+                return break_between(string, space, space+1, width)
+            else
+                return break_between(string, width, width, width)
+            end
+        end
+
+    end
+
+    def break_between(string, start, endpoint, width)
+        string[0...start] + "\n" + word_wrap(string[endpoint..-1], width)
     end
 end
 
@@ -11,3 +30,14 @@ end
 # 'abc123abc123'.sub(/.*\Kabc/, 'ABC')
 # #=> "abc123ABC123"
 
+ # if string.length > width
+        #     if string[width] == " "
+        #         string[width] = "\n"
+        #         return string
+        #     else
+        #         split_string = [string[0..(width)], string[(width + 1)..-1]]
+        #         split_string[0] = split_string[0].sub(/.*\K\s/, "\n")
+        #         return split_string.join
+        #     end
+        # end
+        # string
--- spec/wordwrap_spec.rb
+++ spec/wordwrap_spec.rb
@@ -3,6 +3,12 @@ require_relative "../lib/wordwrap.rb"
 describe Wordwrap do
     context "wraps words" do 
 
+        context "given nil" do
+            it "returns an empty string" do 
+                expect(described_class.new.word_wrap(nil, 5)).to eq("")
+            end
+        end
+
         context "given empty string" do
             it "returns an empty string" do 
                 expect(described_class.new.word_wrap("", 5)).to eq("")
@@ -21,24 +27,35 @@ describe Wordwrap do
             end
         end
 
+        context "given string with length greater than width" do
+            it "returns the split string" do 
+                expect(described_class.new.word_wrap("mountain", 5)).to eq("mount\nain")
+                expect(described_class.new.word_wrap("dinosaur", 4)).to eq("dino\nsaur")
+                expect(described_class.new.word_wrap("verylongword", 4)).to eq("very\nlong\nword")
+            end
+        end
+
         context "given string with space" do
             it "breaks line on space" do 
-                expect(described_class.new.word_wrap("hello world", 5)).to eq("hello\nworld")
+                expect(described_class.new.word_wrap("word word", 6)).to eq("word\nword")
+                expect(described_class.new.word_wrap("wrap here", 6)).to eq("wrap\nhere")
+                expect(described_class.new.word_wrap("word word word", 6)).to eq("word\nword\nword")
             end
         end
 
         context "given string with two space" do
             it "breaks line on second space" do 
                 expect(described_class.new.word_wrap("hi hi world", 5)).to eq("hi hi\nworld")
-                expect(described_class.new.word_wrap("hi hi world", 6)).to eq("hi hi\nworld")
+                #expect(described_class.new.word_wrap("hi hi world", 6)).to eq("hi hi\nworld")
             end
         end
 
-        context "given string with three spaces" do
-            it "breaks line on second space" do 
-                expect(described_class.new.word_wrap("hi hi to yu", 5)).to eq("hi hi\nto yu")
-            end
-        end
+        # context "given string with three spaces" do
+        #     it "breaks line on second space" do 
+        #         expect(described_class.new.word_wrap("hi hi to yu", 5)).to eq("hi hi\nto yu")
+        #         expect(described_class.new.word_wrap("hi hi to yu", 6)).to eq("hi hi\nto yu")
+        #     end
+        # end
 
     end
 end
