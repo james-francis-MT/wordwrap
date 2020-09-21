86e548fd5297ad21671e9c8801e2da23fd17990e
James Francis <james.franics@madetech.com>
2020-09-21 10:20:09 +0100

    passed test to break new line on spce

--- lib/wordwrap.rb
+++ lib/wordwrap.rb
@@ -1,6 +1,9 @@
 class Wordwrap
-    def word_wrap(string)
-        ""
+    def word_wrap(string, width)
+        if string.length > width
+            return string.gsub(" ", "\n")
+        end
+        string
     end
 end
 
--- spec/wordwrap_spec.rb
+++ spec/wordwrap_spec.rb
@@ -2,11 +2,31 @@ require_relative "../lib/wordwrap.rb"
 
 describe Wordwrap do
     context "wraps words" do 
-        context "given nil" do
+
+        context "given empty string" do
             it "returns an empty string" do 
-                expect(described_class.new.word_wrap(nil)).to eq("")
+                expect(described_class.new.word_wrap("", 5)).to eq("")
             end
         end
+
+        context "given letter" do
+            it "returns the same letter" do 
+                expect(described_class.new.word_wrap("a", 5)).to eq("a")
+            end
+        end
+
+        context "given string with same length as width" do
+            it "returns the string" do 
+                expect(described_class.new.word_wrap("hello", 5)).to eq("hello")
+            end
+        end
+
+        context "given string with space" do
+            it "breaks line on space" do 
+                expect(described_class.new.word_wrap("hello world", 5)).to eq("hello\nworld")
+            end
+        end
+
     end
 end
 
